4d5cc02f9021e1aaf8f5866dbb65ca97789cb48b
Victor Souza <vfosouza@gmail.com>
2025-06-10 20:36:42 -0300

    Enhance Twitter setup to read credentials from file and obtain bearer token

--- src/main/scala/com/sundogsoftware/sparkstreaming/Utilities.scala
+++ src/main/scala/com/sundogsoftware/sparkstreaming/Utilities.scala
@@ -13,15 +13,59 @@ object Utilities {
   }
   
   /** Configures Twitter service credentials using twitter.txt in the data directory */
-  def setupTwitter() = {
+  def setupTwitter(): Unit = {
     import scala.io.Source
-    
-    for (line <- Source.fromFile("data/twitter.txt").getLines) {
-      val fields = line.split(" ")
-      if (fields.length == 2) {
-        System.setProperty("twitter4j.oauth." + fields(0), fields(1))
-      }
+    import org.apache.http.client.methods.HttpPost
+    import org.apache.http.impl.client.HttpClients
+    import org.apache.http.util.EntityUtils
+    import org.apache.http.message.BasicNameValuePair
+    import org.apache.http.client.entity.UrlEncodedFormEntity
+    import java.util.{ArrayList, Base64}
+    import java.nio.charset.StandardCharsets
+
+    val file = new java.io.File("data/twitter.txt")
+    if (!file.exists()) throw new Exception("File data/twitter.txt not found")
+
+    // Read credentials from file
+    val lines = Source.fromFile(file).getLines().toList
+    val key = lines.find(_.startsWith("consumerKey=")).map(_.split("=")(1)).orNull
+    val secret = lines.find(_.startsWith("consumerSecret=")).map(_.split("=")(1)).orNull
+
+    if (key == null || secret == null)
+      throw new Exception("Twitter credentials not found in expected format")
+
+    // For compatibility with existing Twitter4J code
+    val accessToken = lines.find(_.startsWith("accessToken=")).map(_.split("=")(1)).orNull
+    val accessTokenSecret = lines.find(_.startsWith("accessTokenSecret=")).map(_.split("=")(1)).orNull
+
+    if (accessToken != null && accessTokenSecret != null) {
+      System.setProperty("twitter4j.oauth.consumerKey", key)
+      System.setProperty("twitter4j.oauth.consumerSecret", secret)
+      System.setProperty("twitter4j.oauth.accessToken", accessToken)
+      System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
     }
+
+    // Generate and store bearer token for Twitter API v2
+    val client = HttpClients.createDefault()
+    val post = new HttpPost("https://api.twitter.com/oauth2/token")
+    val credentials = Base64.getEncoder.encodeToString(s"$key:$secret".getBytes(StandardCharsets.UTF_8))
+
+    post.addHeader("Authorization", s"Basic $credentials")
+    post.addHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+    post.setEntity(new UrlEncodedFormEntity(new ArrayList[BasicNameValuePair]() {
+      add(new BasicNameValuePair("grant_type", "client_credentials"))
+    }))
+
+    val response = client.execute(post)
+    val responseString = EntityUtils.toString(response.getEntity, "UTF-8")
+
+    val bearerToken = scala.util.parsing.json.JSON.parseFull(responseString) match {
+      case Some(map: Map[String, Any]) => map("access_token").toString
+      case _ => throw new Exception("Failed to obtain bearer token")
+    }
+
+    // Store the bearer token for use by other components
+    System.setProperty("twitter.bearer.token", bearerToken)
   }
   
   /** Retrieves a regex Pattern for parsing Apache access logs. */

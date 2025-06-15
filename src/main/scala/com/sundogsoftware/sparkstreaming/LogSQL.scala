88b9adf5bb8d1658b0170cf40c208d653fd1ec3c
Victor Souza <vfosouza@gmail.com>
2025-06-15 17:25:59 -0300

    Feature: Enhance LogSQL to handle empty RDDs and write output to JSON files

--- src/main/scala/com/sundogsoftware/sparkstreaming/LogSQL.scala
+++ src/main/scala/com/sundogsoftware/sparkstreaming/LogSQL.scala
@@ -46,37 +46,34 @@ object LogSQL {
  
     // Process each RDD from each batch as it comes in
     requests.foreachRDD((rdd, time) => {
-      // So we'll demonstrate using SparkSQL in order to query each RDD
-      // using SQL queries.
+      // Skip empty RDDs
+      if (!rdd.isEmpty()) {
+        val spark = SparkSession
+          .builder()
+          .appName("LogSQL")
+          .getOrCreate()
 
-      val spark = SparkSession
-         .builder()
-         .appName("LogSQL")
-         .getOrCreate()
-         
-      import spark.implicits._
+        import spark.implicits._
 
-      // SparkSQL can automatically create DataFrames from Scala "case classes".
-      // We created the Record case class for this purpose.
-      // So we'll convert each RDD of tuple data into an RDD of "Record"
-      // objects, which in turn we can convert to a DataFrame using toDF()
-      val requestsDataFrame = rdd.map(w => Record(w._1, w._2, w._3)).toDF()
+        // Convert RDD to DataFrame
+        val requestsDataFrame = rdd.map(w => Record(w._1, w._2, w._3)).toDF()
 
-      // Create a SQL table from this DataFrame
-      requestsDataFrame.createOrReplaceTempView("requests")
+        // Create common base path and unique output paths
+        val timestamp = time.milliseconds
+        val basePath = "output/LogSQL"
+        val rawDataPath = s"$basePath/requests_$timestamp"
+        val aggregatePath = s"$basePath/agent_counts_$timestamp"
 
-      // Count up occurrences of each user agent in this RDD and print the results.
-      // The powerful thing is that you can do any SQL you want here!
-      // But remember it's only querying the data in this RDD, from this batch.
-      val wordCountsDataFrame =
-        spark.sqlContext.sql("select agent, count(*) as total from requests group by agent")
-      println(s"========= $time =========")
-      wordCountsDataFrame.show()
-      
-      // If you want to dump data into an external database instead, check out the
-      // org.apache.spark.sql.DataFrameWriter class! It can write dataframes via
-      // jdbc and many other formats! You can use the "append" save mode to keep
-      // adding data from each batch.
+        // Write the raw data to JSON files
+        println(s"========= $time =========")
+        println(s"Writing raw data to $rawDataPath")
+        requestsDataFrame.write.json(rawDataPath)
+
+        // Compute and write aggregated data
+        val agentCountsDF = requestsDataFrame.groupBy("agent").count()
+        println(s"Writing aggregated data to $aggregatePath")
+        agentCountsDF.write.json(aggregatePath)
+      }
     })
     
     // Kick it off
